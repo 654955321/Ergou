d3484fc8156e34c6be95222359629f123b81d38c
ergou <654955321@qq.com>
2015-05-23 11:12:03 +0800

    增加 JackisSharp 盲僧

--- Ergou©AD 系列/JackisSharp 盲僧/Properties/AssemblyInfo.cs
+++ Ergou©AD 系列/JackisSharp 盲僧/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("yol0 LeeSin")]
+[assembly: AssemblyTitle("Lee is Back")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("yol0 LeeSin")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("Lee is Back")]
+[assembly: AssemblyCopyright("jackisback")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fa06aab2-60cf-4ee5-bfbe-b57a918534a4")]
+[assembly: Guid("51e54ae6-db01-40a6-acee-10cbea3bb39d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
