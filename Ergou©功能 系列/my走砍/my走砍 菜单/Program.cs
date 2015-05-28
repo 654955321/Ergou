ea631b3976baf4b28728dbd6bdc238862c9883d4
ergou <654955321@qq.com>
2015-05-28 23:01:21 +0800

    my 走砍

---
+++ Ergou©功能 系列/my走砍/my走砍 菜单/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using LeagueSharp;
+using LeagueSharp.Common;
+using myOrbwalker;
+
+namespace myOrbwalker_Menu
+{
+    class Program
+    {
+        private static Menu Config;
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            if (ObjectManager.Player.ChampionName == null) return;
+            Config = new Menu("my走砍", "myOrbwalker", true);
+            var orbwalkerMenu = new Menu("my走砍", "myOrbwalker");
+            MYOrbwalker.AddToMenu(orbwalkerMenu);
+            Config.AddSubMenu(orbwalkerMenu);
+
+            Config.AddToMainMenu();
+            Game.OnUpdate += Game_OnUpdate;
+        }
+
+        private static void Game_OnUpdate(EventArgs args)
+        {
+            if (ObjectManager.Player.IsDead) return;
+        }
+    }
+}
